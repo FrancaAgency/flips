46444956fb80f0b51b8e4fd98c9d309c509810b4
Jp-droid-crypto <72044036+Jp-droid-crypto@users.noreply.github.com>
2021-10-25 09:35:11 -0500

    Commit 25 de octubre
    Commit 25 de octubre

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\dashboard\RankingController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,7 +21,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+})->name('dashboard');*/
 
 
 Route::post('registro', [UsuarioController::class,'store'])->name('registro.store');
